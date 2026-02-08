4ad96a793ef5b3187897621cf8e3b078bb6c1875
Ft-sego <nkabilesego@gmail.com>
2026-02-09 01:02:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: false,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/docdtiwjg/video/upload/v1770304400/Slip_Away_xbowr9.mp3", // Music streaming URL
+        musicUrl: "assets/music/slipaway.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.6                        // Volume level (0.0 to 1.0)
