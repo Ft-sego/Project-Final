ae96d0200d10b2556816cd032896e115f8e6b465
Ft-sego <nkabilesego@gmail.com>
2026-02-09 00:18:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/docdtiwjg/video/upload/J.Cole_She_s_Mine_Pt.1_Official_Audio_uqebyf.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
