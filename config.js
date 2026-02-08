f9c3804addbc9235d711df259bb461a6b85d1b21
Ft-sego <nkabilesego@gmail.com>
2026-02-09 00:10:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/docdtiwjg/video/upload/v1770304400/Slip_Away_xbowr9.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/docdtiwjg/video/upload/v1770588577/J.Cole_She_s_Mine_Pt.1_Official_Audio_uqebyf.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
