99dfea661fb8e00a66fdeee702a68ec2371e69c9
Ft-sego <nkabilesego@gmail.com>
2026-02-09 00:53:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     // Add your own music URL after getting proper licenses
     music: {
         enabled: true,                     // Music feature is enabled
-        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/docdtiwjg/video/upload/v1770304400/Slip_Away_xbowr9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
